8ecae53607b601ab77095292594131a9f24458dd
ccomb <devnull@localhost>
2007-02-25 19:22:55 +0000

    retour à zero pour le minimum_generation

--- generations/__init__.py
+++ generations/__init__.py
@@ -1,7 +1,7 @@
 from zope.app.generations.generations import SchemaManager
 
 ZompatibleSchemaManager = SchemaManager(
-    minimum_generation=2,
+    minimum_generation=0,
     generation=2,
     package_name='zompatible.generations'
 )
