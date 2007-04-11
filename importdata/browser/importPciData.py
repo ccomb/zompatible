564f6fc5074a6a11a9838c83c6a0d3c7548d28b3
arnaud <devnull@localhost>
2007-04-11 13:59:57 +0000

    Removing pci in object names as it also works with usb.

--- importdata/browser/importPciData.py
+++ importdata/browser/importPciData.py
@@ -1,16 +1,16 @@
 from zope.publisher.browser import BrowserPage
 from zope.formlib.form import EditForm, Fields
-from zompatible.importdata.interfaces import IImportPciData
+from zompatible.importdata.interfaces import IImportData
 
-class ViewImportPciData(BrowserPage):
-	""" A simple view of ImportPciData
+class ViewImportData(BrowserPage):
+	""" A simple view of ImportData
 	"""
 	def __call__(self):
 		response = self.request.response
 		response.setHeader('Content-Type', 'text/plain')
 		return self.context.data
 		
-class ImportPciDataEditForm(EditForm):
-	form_fields = Fields(IImportPciData).omit('__parent__') # omit parent because containers constraint adds __parent__ in the schema
-	label = u"Copy pci.ids file data here"
+class ImportDataEditForm(EditForm):
+	form_fields = Fields(IImportData).omit('__parent__') # omit parent because containers constraint adds __parent__ in the schema
+	label = u"Copy file data here"
 	
