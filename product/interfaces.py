eccb32d0bd08bf42db2bb759db666346beea35bf
ccomb <devnull@localhost>
2007-11-09 00:40:16 +0000

    Raw and dirty merge from device and software to product

--- product/interfaces.py
+++ product/interfaces.py
@@ -1,29 +1,147 @@
-from zope.schema import TextLine, List
+# -*- coding: utf-8 -*-
+from zope.app.container.interfaces import IContainer, IContained
+from zope.app.container.constraints import contains
+from zope.schema import List, TextLine, Choice, Text
 from zope.interface import Interface
+from zope.index.text.interfaces import ISearchableText
 
 
-class IProduct(Interface):
+class IDevice(IContained):
+    u"""
+    IDevice offers basic attributes of a device
     """
-    """
-    name = TextLine (
-                    title = u"Name",
-                    description = u"Name of the product",
-                    required = True
-                    )
+    #containers('zompatible.device.interfaces.IDeviceContainer')
+    #contains('zompatible.driver.interfaces.IDriver')
+    names = List(title=u'names', description=u'possible names of the device', min_length=1, value_type=TextLine(title=u'name', description=u'a name for the device (commercial name, code name, etc.)'))
+    organization = Choice(title=u'Organization', description=u'the organization producing this device', source="orgasource", required=False)
+    description = Text(title=u"description", description=u"description of the device", required=False, max_length=1000)
+#    physicalinterfaces = List(title=u'physical interfaces', description=u'list of physical interfaces on the device', value_type=Object(title=u'physical interface', description=u'a physical interface on the device', schema=IPhysicalInterface))
+#    existingdrivers = List(title=u'existing drivers', description=u'list of supported OS', value_type=Object(title=u'supported OS', description=u'OS on which the device works', schema=IDriver))
+    pciid = TextLine(title=u'pci id', description=u'PCI identifier', required=False)
+    usbid = TextLine(title=u'usb id', description=u'USB identifier', required=False)
 
-    """ Sould it be a list or only one category ? (As the product should contain
-         sub products describing each category)
-    """ 
-    categories = List(
-                      title = u"Categories",
-                      description = u"List of categories the product belongs to",
-                      required = False
-                      )
+
+class IDeviceContainer(IContainer, IContained):
+    u"""
+    a toplevel device container. This is a base storage for devices.
+    """
+    contains(IDevice)
     
-    def Display( ):
-        u"""
-        """
-        
-    def DisplayCharacteristics( ):
-        u"""
-        """
+
+
+class ISubDevices(Interface):
+    u"""
+    interface of an object that has subdevices.
+    The source is called as a named utility registered for IVocabularyFactory (see in device.py)
+    """
+    subdevices = List(title=u'subdevices', value_type=Choice(title=u'subdevice', description=u'a subdevice (chip, component)', source="devicesource"), required=False)
+
+
+class ISearchableTextOfDevice(ISearchableText):
+    u"""
+    Marker interface that allows to index devices separately
+    """
+
+class ISoftware(IContained):
+    u"""
+    Base attributes of a software
+    """
+    names = List(title=u'names', description=u'possible software names', min_length=1, value_type=TextLine(title=u'name', description=u'possible software names (commercial name, code name, etc.'))
+    organization = Choice(title=u'Organization', description=u'the organization producing this software', source="orgasource", required=False)
+    #architectures = List(title=u'architectures', description=u'architectures that software applies to', value_type=Object(title=u'architecture',description=u'list of architectures', schema=IArchitecture))
+    version = TextLine(title=u'version', description=u'a text string describing the version', required=True)
+    builtVersion = TextLine(title=u'built version', description=u'built version', required=False)
+    codename=TextLine(title=u'code name (if any)', description=u'the code name of the software', required=False)
+    description=Text(title=u'Description', description=u'Software description', required=False)
+    #license = Choice......(title=u'which license?', description=u'the licence of the software', schema=ILicense)
+    #features = List(title=u'features', description=u'list of features of the driver', value_type=Object(title=u'feature', description=u'a feature of the driver', schema=IFeature))
+    #stabilityreports = List(title=u'stability levels', description=u'provided stability levels', value_type=Object(title=u'stability level',description=u'stability level', schema=IStabilityReport))
+    link = URI(title=u'a link to software', description=u'link to the software', required=False)
+
+class IFuzzy(Interface):
+    u"""
+    interface for an imprecise software
+    """
+    group = List(title=u'variations', description=u'variations of the software', min_length=1, value_type=Choice(title=u"variation", source="softwaresource"))
+
+class ISubSoftware(Interface):
+    u"""
+    interface of an object that has subsoftware.
+    The source is called as a named utility registered for IVocabularyFactory (see in software.py)
+    """
+    subsoftware = List(title=u'subsoftware', value_type=Choice(title=u'subsoftware', description=u'a subsoftware (kernel, driver, library, etc.)', source="softwaresource"), required=False)
+
+
+class ISoftwareContainer(IContainer, IContained):
+    u"""
+    L'interface du dossier racine qui contient les OS
+    """
+    contains(ISoftware)
+
+class ISearchableTextOfSoftware(ISearchableText):
+    u"""
+    on déclare un index juste pour cette interface de façon à indexer juste les software
+    """
+
+
+
+
+
+
+# ce qui est dessous ne sert pas pour l'instant
+
+
+class ILicense(Interface):
+    u"""
+    un objet licence (GPL 2.0 est un objet licence, GPL 3.0 un autre)
+    """
+    name = TextLine(title=u'license name', description=u'the name of the license')
+    version = TextLine(title=u'license version', description=u'the version of the license')
+    free = Bool(title=u'real free software licence?')
+    terms = Text(title=u'license terms', description=u'the terms of the license')
+
+
+class IPatchableSoftware(ISoftware):
+    u"""
+    When a software is patched over an original, the original receives this interface
+    to keep the list of derived software
+    """
+    patched = List(title=u'derivatives', description=u'existing patched versions of this software', value_type=Object(ISoftware, title=u'derivative'))
+    
+class IPatchedSoftware(ISoftware):
+    u"""
+    This interface can be used to create all flavours of the kernel : (ex: 2_ubuntu1).
+    the implementation would link to the original software (aka upstream)
+    An original kernel must be created, then all distribution specific flavours must provide this interface.
+    This interface must not be user for real forks, but only patched/modified software.
+    Real forks must become independant objects
+    """
+    upstream = Object(title=u'patched over', description=u'original software from which this one was derived', schema=ISoftware)
+
+  
+class IDriver(ISoftware):
+    u"""
+    a driver for a software
+    we must think of how to speak about ndiswrapper, which is not a driver
+    And a driver can apply to a kernel, or be in userspace for xorg. So... ?
+    We must express that a linux driver may exist for a device, but not be included in a distro, or in the kernel
+    In hardware reviews, we can often find references to kernel versions, so we should include IKernel?
+    a driver is included in the kernel, and can be added by a distro
+    so a driver applies to a kernel AND a distro
+    We must express that the fact that distroX use kernelY automatically brings POSSIBLE support of a particular device
+    a distro contains a kernel, that contains a driver
+    The features of the driver should be a subset of the features of the device
+    subset : one of the device features is not accessible from the computer, or a feature is not implemented in the driver.
+    superset : the driver offers a feature purely by software (ex: winmodem) (?)
+    """
+    subsystems = List(title=u'for which subsystems?', description=u'the driver is for which subsystems? (kernel, xorg, ...)', value_type=Object(title=u'subsystem', description=u'subsystem this driver applies to', schema=(ISoftware)))
+
+class IInclusionLevel(Interface):
+    u"""
+    tells how software is included in the distro
+    For Debian, inclusion can be main, contrib, non-free, etc.
+    A REVOIR !
+    """
+    software = Object(title=u'os', description=u'os', schema=ISoftware)
+    inclusion=TextLine(title=u'included', description=u'inclusion type')
+
--- product/product.py
+++ product/product.py
@@ -72,3 +72,315 @@ productFactory = Factory(
                          )
 
         
+# -*- coding: utf-8 -*-
+from zope.interface import implements
+from persistent import Persistent
+from zope.app.folder.folder import Folder
+from zope.component import adapts, adapter
+from zope.schema.interfaces import ISource, IVocabularyFactory
+from zope.app.component.hooks import getSite
+from zope.app.container.interfaces import INameChooser, IObjectRemovedEvent
+from zope.app.container.contained import NameChooser
+from zope.component.factory import Factory
+from zope.copypastemove import ObjectMover
+
+import string
+from BTrees.OOBTree import OOBTree
+
+from interfaces import *
+
+@adapter(IDevice, IObjectRemovedEvent)
+def DeviceRemovedEvent(device, event):
+    u"a subscriber that put the device into trash if it contains support objects, intead of deleting it"
+    if event.newParent is None and len(device.supports) != 0 :
+        trash = getSite()['trash']
+        device_name = INameChooser(trash).chooseName(u"",device)
+        trash[device_name]=device
+
+class DeviceContainer(Folder):
+    """
+    a folder that contains devices
+    """
+    __name__=__parent__= None
+    implements(IDeviceContainer)
+
+class Device(Persistent):
+    implements(IDevice, ISubDevices)
+    names=[]
+    subdevices=[]
+    description = u""
+    pciid=""
+    usbid=""
+    # IDevice fournit IContained donc il faut mettre ces attributs :
+    __name__=__parent__= None
+    def __init__(self, names=None, description=None):
+        u"the list of supported software that lead to the Support objects"
+        self.names = names
+        self.description = description
+        self.supports = OOBTree()
+        super(Device, self).__init__()
+    def __getattr__(self, name):
+        if name == 'organization':
+            if self.__parent__ is not None:
+                return self.__parent__.__parent__
+            return None
+        return super(Device, self).__getattr__(name)
+    def __setattr__(self, name, value):
+        if name == 'organization':
+            if value is not self.__parent__ and value is not None and self.__parent__ is not None:
+                mover = ObjectMover(self)
+                if not mover.moveableTo(value['devices']):
+                    raise "Impossible action"
+                else:
+                    mover.moveTo(value['devices'])
+        else:
+            super(Device, self).__setattr__(name, value)
+        
+deviceFactory = Factory(
+    Device,
+    title=u"Device factory",
+    description = u"This factory instantiates new Device."
+    )
+    
+class DeviceNameChooser(NameChooser):
+    u"""
+    adapter qui permet de choisir le nom du device auprès du container
+    Le vrai nom est stocké dans un attribut, mais ce nom est aussi important
+    car il apparaît dans l'URL, et sert pour le traversing.
+    """
+    implements(INameChooser)
+    adapts(IDevice)
+    def chooseName(self, name, device):
+        if not name and device is None:
+            raise "DeviceNameChooser Error"
+        rawname = name
+        if device is not None and len(device.names)>0:
+            rawname = device.names[0]
+        return string.lower(rawname).strip().replace(' ','-').replace(u'/',u'-').lstrip('+@') # UTILISER PEUT-ÊTRE UNICODE.LOWER*********************
+    def checkName(self, name, device): # **************************utiliser le super.checkName ) la place
+        if name in device.__parent__ and device is not device.__parent__['name']:
+            return False
+        else :
+            return True
+
+# Autre méthode pour créer un objectwidget ? (pas encore testé)
+#class DeviceWidget(ObjectWidget):
+#    __used_for__ = IDevice
+#    subdevices_widget = CustomWidgetFactory(ObjectWidget, Device)
+
+class SearchableTextOfDevice(object):
+    u"""
+    l'adapter qui permet d'indexer les devices. Il fournit le texte à indexer depuis le contenu d'un objet device.
+    Le principe est génial car ça permet notamment d'indexer n'importe quels attributs,
+    et même les traductions qui se trouvent dans des fichiers .po externes !!
+    """
+    implements(ISearchableTextOfDevice)
+    adapts(IDevice)
+    def __init__(self, context):
+        self.context = context
+    def getSearchableText(self):
+        u"peut probablement être optimisé"
+        sourcetext = texttoindex = u''
+        u"on commence par créer un texte depuis tous les noms"
+        for word in self.context.names:
+            sourcetext += word + " "
+        u"""puis on génère les sous-mots pour pouvoir rechercher les parties des noms
+        par exemple, foobar donnera : foobar oobar obar bar ar r"""
+        for word in sourcetext.split():        
+            for subword in [ word[i:] for i in xrange(len(word)) if len(word)>=1 ]:
+                texttoindex += subword + " "
+        return texttoindex
+
+class DeviceSource(object):
+    """
+    implémentation de la Source de Devices utilisée dans le schema de sub_devices.
+    Il s'agit juste d'implémenter ISource
+    Cette source est censée représenter l'ensemble de tous les devices.
+    Elle parcourt toutes les organizations et recherche le device voulu.
+    *********
+    ATTENTION, dans l'implémentation ci-dessous, si deux orga ont
+    des devices avec le même nom, seul le 1er est retourné.
+    """
+    implements(ISource)
+    def __contains__(self, value):
+        root = getSite()
+        for orga in root['organizations']:
+            if 'devices' in root['organizations'][orga] and value.__name__ in root['organizations'][orga]['devices'].keys():
+                return True
+        return False
+
+class DeviceVocabularyFactory(object):
+    implements(IVocabularyFactory)
+    def __call__(self, context):
+        return DeviceSource()
+
+#class FuzzyDevice(Device):
+    #u"""
+    #A class that represents a device or another, for people that don't
+    #really know which one to choose. This just a container for a series of
+    #devices that are the same, or in which people don't really see the difference
+    #or that are managed by the same driver
+    #"""
+    #implements(IDevice, ISupported)
+    #names = []
+    ##organization = None
+    #description = u""
+    #devices = []
+    #supports
+    
+
+
+
+
+# -*- coding: utf-8 -*-
+from persistent import Persistent
+from zope.app.folder.folder import Folder
+from zope.interface import implements
+from zope.component import adapts, adapter
+from zope.app.container.interfaces import INameChooser, IObjectRemovedEvent
+from zope.app.container.contained import NameChooser
+from zope.app.component.hooks import getSite
+from zope.copypastemove import ObjectMover
+from zope.component.factory import Factory
+import string
+from zope.schema.interfaces import ISource, IVocabularyFactory
+from BTrees.OOBTree import OOBTree
+
+from interfaces import *
+
+
+@adapter(ISoftware, IObjectRemovedEvent)
+def SoftwareRemovedEvent(software, event):
+    u"a subscriber that put software to trash if it contains support objects, before deleting it"
+    if event.newParent is None and len(software.supports) != 0 :
+        trash = getSite()['trash']
+        software_name = INameChooser(trash).chooseName(u"",software)
+        trash[software_name]=software
+
+class SoftwareContainer(Folder):
+    "a container for all software"
+    implements(ISoftwareContainer)
+    __name__=__parent__=None
+        
+class Software(Persistent):
+    implements(ISoftware, ISubSoftware)
+    builtVersion = u""
+    description = u""
+    subsoftware = []
+    names = architectures = []
+    version = codename = u""
+    link = u""
+    url=""
+    __name__=__parent__=None
+    def __init__(self, names=None, description=None):
+        u"a list of support devices, that lead to the Support objects"
+        self.names = names
+        self.description = description
+        self.supports = OOBTree()
+        super(Software, self).__init__()
+    def __getattr__(self, name):
+        if name == 'organization':
+            if self.__parent__ is not None:
+                return self.__parent__.__parent__
+            return None
+        return super(Software, self).__getattr__(name)
+    def __setattr__(self, name, value):
+        if name == 'organization':
+            if value is not self.__parent__ and value is not None and self.__parent__ is not None:
+                mover = ObjectMover(self)
+                if not mover.moveableTo(value['software']):
+                    raise "Impossible action"
+                else:
+                    mover.moveTo(value['software'])
+        else:
+            super(Software, self).__setattr__(name, value)
+
+        
+softwareFactory = Factory(
+    Software,
+    title=u"Software factory",
+    description = u"This factory instantiates new Software."
+    )
+    
+class SoftwareNameChooser(NameChooser):
+    u"""
+    adapter that allows to choose the name of the Software from the container point of view
+    The real name is stored in an attribute, but this name is important
+    as it appears in the URL and is used for traversing
+    """
+    implements(INameChooser)
+    adapts(ISoftware)
+    def chooseName(self, name, software):
+        if not name and software is None:
+            raise "SoftwareNameChooser Error"
+        rawname = name
+        if software is not None and len(software.names)>0:
+            rawname = software.names[0]
+        codename = version = u""
+        if software.codename:
+            codename="-" + software.codename
+        if software.version:
+            version = software.version
+        return string.lower(rawname + "-" + version + codename).strip().replace(' ','-').replace(u'/',u'-').lstrip('+@')
+    def checkName(self, name, software):
+        if name in software.__parent__ and software is not software.__parent__['name']:
+            return False
+        else :
+            return true
+
+class SearchableTextOfSoftware(object):
+    u"""
+    The adapter that allows to index software objects
+    """
+    implements(ISearchableTextOfSoftware)
+    adapts(ISoftware)
+    def __init__(self, context):
+        self.context = context
+    def getSearchableText(self):
+        sourcetext = texttoindex = u''
+        u"First, gather all interesting text"
+        for word in self.context.names + [ self.context.version, self.context.codename ] :
+            sourcetext += word + " "
+        u"then split all words into subwords"
+        for word in sourcetext.split():        
+            for subword in [ word[i:] for i in xrange(len(word)) if len(word)>=1 ]:
+                texttoindex += subword + " "
+        return texttoindex
+
+class SoftwareSource(object):
+    """
+    implémentation de la Source de Software utilisée dans le schema de Support
+    Il s'agit juste d'implémenter ISource
+    Cette source est censée représenter l'ensemble de tous les softwares.
+    Elle parcourt toutes les organizations et recherche le software voulu.
+    *********
+    ATTENTION, dans l'implémentation ci-dessous, si deux orga ont
+    des softwares avec le même nom, seul le 1er est retourné.
+    """
+    implements(ISource)
+    def __contains__(self, value):
+        root = getSite()
+        for orga in root['organizations']:
+            if 'software' in root['organizations'][orga] and value.__name__ in root['organizations'][orga]['software'].keys():
+                return True
+        return False
+
+class SoftwareVocabularyFactory(object):
+    implements(IVocabularyFactory)
+    def __call__(self, context):
+        return SoftwareSource()
+
+class FuzzySoftware(Software):
+    u"""
+    a software representing several softwares
+    For ex : ubuntu representing all variants (kubuntu, ubuntu server, etc.)
+    It contains the union of all support objects an can have its own supports.
+    """
+    implements(IFuzzy)
+    group = []
+    
+
+
+
+
+
