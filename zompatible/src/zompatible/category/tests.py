05430b8227049a402138c9ee0c9f8e8364c80892
ccomb <devnull@localhost>
2008-03-12 10:59:15 +0000

    register an intid for category tests

--- zompatible/src/zompatible/category/tests.py
+++ zompatible/src/zompatible/category/tests.py
@@ -4,11 +4,15 @@ import unittest
 import doctest
 from category import AvailableCategories
 from zope.app.testing import placelesssetup
-from zope.component import provideAdapter
+from zope.component import provideAdapter, provideUtility
+from zope.app.intid import IntIds
+from zope.app.intid.interfaces import IIntIds
 
 def setUp(test):
     #placelesssetup.setUp(test)
     provideAdapter(AvailableCategories)
+    intids = IntIds()
+    provideUtility(intids, IIntIds)
 
 def test_suite( ):
     # we create a testsuite
